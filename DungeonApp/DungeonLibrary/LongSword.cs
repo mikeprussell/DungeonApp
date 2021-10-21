3adf75cf4972f04f8eaa2cc047b7af8e4216ff14
mikeprussell <mikeprussell@outlook.com>
2021-10-21 08:12:58 -0500

    Kill count logic added for 3 and 4

---
+++ DungeonApp/DungeonLibrary/LongSword.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    public class LongSword : Weapon
+    {
+
+        public bool HasShield { get; set; }
+
+        public LongSword(string name, int bonusHitChance, int minDamage, int maxDamage,
+            bool isTwoHanded, string description)
+            : base(name, bonusHitChance, minDamage, maxDamage, isTwoHanded)
+        {
+            
+        }
+
+    }
+
+}
